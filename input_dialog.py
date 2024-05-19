469c4d1a2df09de665f8cc17de056022b4a44c2c
likanug <1452930742@qq.com>
2024-05-19 09:26:34 +0800

    新增"字体选择框"的示例代码

--- input_dialog.py
+++ input_dialog.py
@@ -1,6 +1,7 @@
 from PyQt6.QtGui import QColor
 from PyQt6.QtWidgets import (QWidget, QPushButton, QLineEdit,
-                             QInputDialog, QApplication, QFrame, QColorDialog)
+                             QInputDialog, QApplication, QFrame, QColorDialog, QLabel, QFontDialog, QSizePolicy,
+                             QVBoxLayout)
 import sys
 
 
@@ -64,10 +65,46 @@ class ColorDialogSample(QWidget):
                                    % col.name())
 
 
+class FontDialogSample(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.lbl = None
+        self.initUI()
+
+    def initUI(self):
+        vbox = QVBoxLayout()
+
+        btn = QPushButton('Dialog', self)
+        btn.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        btn.move(20, 20)
+
+        vbox.addWidget(btn)
+
+        btn.clicked.connect(self.showDialog)
+
+        self.lbl = QLabel('Knowledge only matters', self)
+        self.lbl.move(130, 20)
+
+        vbox.addWidget(self.lbl)
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 450, 350)
+        self.setWindowTitle('Font dialog')
+        self.show()
+
+    def showDialog(self):
+        font, ok = QFontDialog.getFont()
+
+        if ok:
+            self.lbl.setFont(font)
+
+
 def main():
     app = QApplication(sys.argv)
     # ex = InputDialogSample()
     # color = ColorDialogSample()
+    font = FontDialogSample()
     sys.exit(app.exec())
 
 
