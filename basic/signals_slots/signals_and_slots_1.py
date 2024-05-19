7d24f12b51e55baa01c5d88f5a1e560ae66a2138
likanug <1452930742@qq.com>
2024-05-19 18:01:45 +0800

    给QWidget添加状态，然后信号插槽传递状态信息

--- basic/signals_slots/signals_and_slots_1.py
+++ basic/signals_slots/signals_and_slots_1.py
@@ -6,21 +6,22 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.button_is_checked = False
         self.setWindowTitle("My app")
         button = QPushButton("Press Me!")
         button.setCheckable(True)
         # 给clicked 的signal添加slot the_button_was_clicked
         # 点击button后控制台会输出'Clicked'
-        button.clicked.connect(self.the_button_was_clicked)
+        # button.clicked.connect(self.the_button_was_clicked)
         # 添加 toggled的信号槽传递checked状态
         button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
         self.setCentralWidget(button)
 
-    def the_button_was_clicked(self):
-        print("Clicked")
-
     def the_button_was_toggled(self, checked):
-        print("Checked?", checked)
+        self.button_is_checked = checked
+        print(self.button_is_checked)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
