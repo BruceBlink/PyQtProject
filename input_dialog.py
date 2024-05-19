4e2f803c8b711a48a9242884c1d89e73d1e6bf79
likanug <1452930742@qq.com>
2024-05-19 09:22:50 +0800

    新增"颜色选择框"的示例代码

--- input_dialog.py
+++ input_dialog.py
@@ -1,5 +1,6 @@
+from PyQt6.QtGui import QColor
 from PyQt6.QtWidgets import (QWidget, QPushButton, QLineEdit,
-                             QInputDialog, QApplication)
+                             QInputDialog, QApplication, QFrame, QColorDialog)
 import sys
 
 
@@ -30,9 +31,43 @@ class InputDialogSample(QWidget):
             self.le.setText(str(text))
 
 
+class ColorDialogSample(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.frm = None
+        self.btn = None
+        self.initUI()
+
+    def initUI(self):
+        col = QColor(0, 0, 0)
+
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+
+        self.btn.clicked.connect(self.showDialog)
+
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }"
+                               % col.name())
+        self.frm.setGeometry(130, 22, 200, 200)
+
+        self.setGeometry(300, 300, 450, 350)
+        self.setWindowTitle('Color dialog')
+        self.show()
+
+    def showDialog(self):
+        col = QColorDialog.getColor()
+
+        if col.isValid():
+            self.frm.setStyleSheet("QWidget { background-color: %s }"
+                                   % col.name())
+
+
 def main():
     app = QApplication(sys.argv)
-    ex = InputDialogSample()
+    # ex = InputDialogSample()
+    # color = ColorDialogSample()
     sys.exit(app.exec())
 
 
