f637c6f33fdd18c0ebb6ace258e051b2e1bc29a2
likanug <1452930742@qq.com>
2024-05-19 19:48:13 +0800

    定义类变量传递button的checked状态

---
+++ basic/signals_slots/signals_and_slots_1d.py
@@ -0,0 +1,28 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.button_is_checked = True
+        self.setWindowTitle("My app")
+
+        self.button = QPushButton("Press Me!")
+        self.button.setCheckable(True)
+
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
+        self.setCentralWidget(self.button)
+
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+        print(self.button_is_checked)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
